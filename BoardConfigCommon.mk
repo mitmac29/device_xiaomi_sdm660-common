d3241755117cade21c6a9752a67af8a8e4cfca0b
mitmac29 <118977337+mitmac29@users.noreply.github.com>
2026-02-17 01:13:09 +0530

    Update SELinux policy include path for libperfmgr

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,7 +218,7 @@ SOONG_CONFIG_XIAOMI_SDM660_PARTITION_SCHEME := a
 endif
 
 # SELinux
-include device/lineage/sepolicy/libperfmgr/sepolicy.mk
+include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
